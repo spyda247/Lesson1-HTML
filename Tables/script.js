1c3fd93b38314a1a096c4f132534a64689851cef
Babajide Ibiayo <babajideibiayo@yahoo.com>
2023-08-15 15:48:38 +0100

    modified script.js for Tables

--- Tables/script.js
+++ Tables/script.js
@@ -1,20 +1,15 @@
-console.log('Script initialised....');
-
+// Main
 const totalElement = document.querySelector('#tbl-total');
 
-const prices = document.querySelectorAll('.price');
+const prices = Array.from(document.querySelectorAll('.price'));
 
+totalElement.textContent = calculateTotal(prices);
+
+// Utitlity
 function calculateTotal(list) {
     let total = 0;
-    for(let i = 0; i < list.length; i++) {
+    for (let i = 0; i < list.length; i++) {
         total = total + parseFloat((list[i].textContent), 10);
     }
-    return total;
 }
-
-const result = calculateTotal(Array.from(prices));
-
-totalElement.textContent = result;
-
-
-//console.log(Array.from(prices));
+    return total;
